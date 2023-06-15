d79283b685ce24fb68ca722a024191ec8548c01c
AMurillo.365 <AMurillo.365@provmazdevtest>
2023-06-15 17:10:50 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Openshift_GitHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
